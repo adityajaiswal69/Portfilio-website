e6a6cffce0cb5b01515b846a3020fbf6e788c13e
Aditya Jaiswal <159708103+adityajaiswal69@users.noreply.github.com>
2024-08-06 17:03:16 +0530

    my sevices

--- script.js
+++ script.js
@@ -3,18 +3,3 @@ var typed = new Typed('#element', {
     typeSpeed: 50,
     loop:true,
   });
-
-  var tablinks = document.getElementsByClassName("tab-links");
-  var tabcontents = document.getElementsByClassName("tab-contents");
-  function opentab(event,tabname){    
-    for( let tablink of tablinks){
-      tablink.classList.remove("active-link")
-    }
-    for( let tabcontent of tabcontents){
-      tabcontent.classList.remove("active-tab")
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add(" active-tab");
-    
-
-  } 
