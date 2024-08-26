5d37fdddf1c8535b8858b176cb7b144d35c92675
Aditya Jaiswal <159708103+adityajaiswal69@users.noreply.github.com>
2024-08-26 09:36:09 +0530

    form

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var typed = new Typed('#element', {
     loop:true,
   });
 console.log("welcome! to My portfolio");
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwVNjjsSMshjQIugxiZvp09l79Fe4l2YR9mfCd70YnyTgvwUFnEAGjdxGFQvezmJEzVog/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
