cd0b1e85b5695ab373a064e0b780dc25d4c6e378
Aditya Jaiswal <159708103+adityajaiswal69@users.noreply.github.com>
2024-09-06 17:16:01 +0530

    adding netfilix

--- script.js
+++ script.js
@@ -5,12 +5,3 @@ var typed = new Typed('#element', {
   });
 console.log("welcome! to My portfolio");
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwVNjjsSMshjQIugxiZvp09l79Fe4l2YR9mfCd70YnyTgvwUFnEAGjdxGFQvezmJEzVog/exec'
-  const form = document.forms['submit-to-google-sheet']
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
